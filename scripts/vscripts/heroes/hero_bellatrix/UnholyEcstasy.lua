8b14a6555daf0fc5639aff94bebd35cd16129ea2
Wouterz90 <wouter.zegers@gmail.com>
2015-11-06 20:16:09 +0100

    Update UnholyEcstasy.lua

--- scripts/vscripts/heroes/hero_bellatrix/UnholyEcstasy.lua
+++ scripts/vscripts/heroes/hero_bellatrix/UnholyEcstasy.lua
@@ -1,3 +1,4 @@
+-- Rip and Tear functions are here because I need to pass the damage value over from Unholy Ecstasy.
 function UnholyEcstasy (keys)
 	local caster = keys.caster
 	local ability = keys.ability
@@ -9,23 +10,95 @@ function UnholyEcstasy (keys)
 		DamageCollection = 0
 	end
 		  DamageCollection = DamageCollection + (damageTaken*damagereduction)
-	print(DamageCollection)
+	print(target)
 	target:SetHealth(target:GetHealth() + (damageTaken*damagereduction ))
+
+	local damagecap= ability:GetLevelSpecialValueFor("damage_cap", ability:GetLevel() - 1 )
+	damagedone = DamageCollection * bonusdamage * 0.01
+	if damagedone > damagecap then
+		damagedone = damagecap
+	end
 end
 
 function BonusDamage (keys)
-	local target = keys.target
+	local target = keys.attacker
 	local unit = keys.caster
 	local ability = keys.ability
 	local bonusdamage = ability:GetLevelSpecialValueFor("bonus_damage", ability:GetLevel() - 1 )
+
+	print(target)
+
 	local damage_table = {}
 	damage_table.ability = ability
 	damage_table.victim = target
 	damage_table.attacker = unit
-	damage_table.damage = DamageCollection * bonusdamage * 0.01
+	damage_table.damage = damagedone
 	damage_table.damage_type = DAMAGE_TYPE_MAGICAL
 	
 	ApplyDamage(damage_table)
 	unit:RemoveModifierByName("Unholy_Ecstasy_Bonus_Damage")
 end
 
+function FirstStrike(keys)
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.target
+	local int_caster = caster:GetIntellect()
+	local int_multiplier = ability:GetLevelSpecialValueFor( "int_multiplier_tooltip", ( ability:GetLevel() - 1 ) )
+	local ini_damage = ability:GetLevelSpecialValueFor("ini_damage", ability:GetLevel() -1 )
+	local heal = ability:GetLevelSpecialValueFor("damage_healed", ability:GetLevel() -1 )
+	if damagedone == nil then
+		damagedone = 0
+	end
+
+	
+	if caster:HasModifier("Unholy_Ecstasy_Bonus_Damage") then
+		local damage_table = {}
+		damage_table.ability = ability
+		damage_table.victim = target
+		damage_table.attacker = caster 
+		damage_table.damage =((int_caster * int_multiplier)+ ini_damage) + damagedone
+		damage_table.damage_type = ability:GetAbilityDamageType()
+		ApplyDamage(damage_table)
+	else
+		local damage_table = {}
+		damage_table.ability = ability
+		damage_table.victim = target
+		damage_table.attacker = caster
+		damage_table.damage =((int_caster * int_multiplier)+ ini_damage)
+		damage_table.damage_type = ability:GetAbilityDamageType()
+		ApplyDamage(damage_table)
+	end
+
+	
+	if caster:HasModifier("Unholy_Ecstasy_Bonus_Damage") then
+		caster:SetHealth( caster:GetHealth() + ((int_caster * int_multiplier)+ ini_damage + damagedone) * heal * 0.01 )
+		caster:RemoveModifierByName("Unholy_Ecstasy_Bonus_Damage")
+	else
+		caster:SetHealth( caster:GetHealth() + ((int_caster * int_multiplier)+ ini_damage ) * heal * 0.01 )
+	end
+end
+
+function SecondStrike(keys)
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.target
+	local int_caster = caster:GetIntellect()
+	local int_multiplier = ability:GetLevelSpecialValueFor("int_multiplier_tooltip", ability:GetLevel() -1 )
+	local ini_damage = ability:GetLevelSpecialValueFor("ini_damage", ability:GetLevel() -1 )
+	local heal = ability:GetLevelSpecialValueFor("damage_healed", ability:GetLevel() -1 )
+	caster:Stop()
+    if caster:IsHexed() or caster:IsStunned() or caster:IsRooted() or caster:IsOutOfGame() then
+    else
+		local damage_table = {}
+		damage_table.ability = ability
+		damage_table.victim = target
+		damage_table.attacker = caster
+		damage_table.damage = ((int_caster 	* int_multiplier)+ ini_damage)
+		damage_table.damage_type = ability:GetAbilityDamageType()
+	
+		ApplyDamage(damage_table)
+
+		caster:SetHealth( caster:GetHealth() + ((int_caster * int_multiplier)+ ini_damage) * heal * 0.01 )
+	end
+end	
